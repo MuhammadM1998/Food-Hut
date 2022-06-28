3c7d5abaacdea88e68bbcdebc6256169482f54c0
MuhammadM1998 <MuhammadM.1097@gmail.com>
2022-06-28 06:40:29 +0200

    Update Meta Tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
     head: {
       titleTemplate: 'Food Hut - %s',
       htmlAttrs: { lang: 'en' },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/public/favicon.ico' },
+      ],
+
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
@@ -12,24 +16,37 @@ export default defineNuxtConfig({
           content:
             "Nuxt 3 & Tailwind 3 version of Forkify App from Jonas Schmedtmann's JavaScript Course.",
         },
-        // Open Graph Thumbnail
-        {
-          property: 'og:image',
-          content:
-            'https://github.com/MuhammadM1998/Food-Hut/tree/main/public/Website-Screenshot.png',
-        },
-        { property: 'og:image:width', content: '1200' },
-        { property: 'og:image:height', content: '627' },
+        // Facebook Meta Tags
+        { property: 'og:url', content: 'https://foodhut.vercel.app' },
+        { property: 'og:type', content: 'website' },
         { property: 'og:title', content: 'Food Hut' },
         {
           property: 'og:description',
           content:
             "Nuxt 3 & Tailwind 3 version of Forkify App from Jonas Schmedtmann's JavaScript Course.",
         },
-        { property: 'og:type', content: 'website' },
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/public/favicon.ico' },
+        {
+          property: 'og:image',
+          content:
+            'https://github.com/MuhammadM1998/Food-Hut/raw/main/public/Website-Screenshot.png',
+        },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '627' },
+        // Twitter Meta Tags
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:domain', content: 'https://foodhut.vercel.app' },
+        { property: 'twitter:url', content: 'https://foodhut.vercel.app/' },
+        { property: 'twitter:title', content: 'Food Hut' },
+        {
+          property: 'twitter:description',
+          content:
+            "Nuxt 3 & Tailwind 3 version of Forkify App from Jonas Schmedtmann's JavaScript Course.",
+        },
+        {
+          property: 'twitter:image',
+          content:
+            'https://github.com/MuhammadM1998/Food-Hut/raw/main/public/Website-Screenshot.png',
+        },
       ],
     },
   },

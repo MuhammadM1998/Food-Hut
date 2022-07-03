5479d44110e8e01f5cf8cf4f82ec5cffbd2b1763
MuhammadM1998 <MuhammadM.1097@gmail.com>
2022-07-03 07:08:21 +0200

    Updated facebook & twitter meta tags, hopefully for the last time ffs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,9 +24,14 @@ export default defineNuxtConfig({
             "Nuxt 3 & Tailwind 3 version of Forkify App from Jonas Schmedtmann's JavaScript Course.",
         },
         // Facebook Meta Tags
+        { property: 'fb:app_id', content: '966242223397117' },
         { property: 'og:url', content: 'https://foodhut.vercel.app' },
         { property: 'og:type', content: 'website' },
-        { property: 'og:title', content: 'Food Hut' },
+        {
+          property: 'og:title',
+          content: 'Find your next amazing Recipe now on Food Hut',
+        },
+        { property: 'og:site_name', content: 'Food Hut' },
         {
           property: 'og:description',
           content:
@@ -37,8 +42,14 @@ export default defineNuxtConfig({
           content:
             'https://res.cloudinary.com/cloud-m98/image/upload/c_scale,h_630,q_40,w_1200/v1656823565/Food%20Hut/Website-Screenshot_hrbeqp.png',
         },
+        {
+          property: 'og:image:secure_url',
+          content:
+            'https://res.cloudinary.com/cloud-m98/image/upload/c_scale,h_630,q_40,w_1200/v1656823565/Food%20Hut/Website-Screenshot_hrbeqp.png',
+        },
         { property: 'og:image:width', content: '1200' },
         { property: 'og:image:height', content: '630' },
+        { property: 'og:image:alt', content: 'Food Hut Website' },
         // Twitter Meta Tags
         { name: 'twitter:card', content: 'summary_large_image' },
         { property: 'twitter:domain', content: 'https://foodhut.vercel.app' },
@@ -52,7 +63,7 @@ export default defineNuxtConfig({
         {
           property: 'twitter:image',
           content:
-            'https://github.com/MuhammadM1998/Food-Hut/raw/main/public/Website-Screenshot.png',
+            'https://res.cloudinary.com/cloud-m98/image/upload/c_scale,h_630,q_40,w_1200/v1656823565/Food%20Hut/Website-Screenshot_hrbeqp.png',
         },
       ],
     },
